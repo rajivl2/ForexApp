ab9d4663e7aa08e25e9b8454627afc9867cda23c
Ford Labs <fordlabs@MGC000010558>
2019-07-31 14:31:31 +0530

    First Project Stucture Commit

---
+++ ForexApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ForexApp
+//
+//  Created by Ford Labs on 31/07/19.
+//  Copyright © 2019 Ford Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
